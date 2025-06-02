be1fe001dec7a0b63243ea7a7b27a2a195510ba7
Glitch-guy0 <prajwalm9845@gmail.com>
2025-06-02 16:25:07 +0530

    kafka admin creating topics

---
+++ src/admin/main.py
@@ -0,0 +1,41 @@
+from kafka.admin import new_topic, KafkaAdminClient
+from kafka.errors import TopicAlreadyExistsError
+
+
+try:
+    print("Connecting to Kafka as Admin...")
+    kafka = KafkaAdminClient(
+        bootstrap_servers='192.168.29.55:9092',
+        client_id='admin_client'
+    )
+    print("Connected to Kafka successfully.")
+except Exception as e:
+    print(f"Failed connecting to kafka: {e}")
+    exit(1)
+
+print("Creating topics...")
+help1 = new_topic.NewTopic(
+    name='help1',
+    num_partitions=1,
+    replication_factor=1
+)
+help2 = new_topic.NewTopic(
+    name='help2',
+    num_partitions=1,
+    replication_factor=1
+)
+try:
+    kafka.create_topics(
+        new_topics=[help1, help2],
+        validate_only=False
+    )
+    print("Topics created successfully.")
+except Exception as e:
+    if isinstance(e, TopicAlreadyExistsError):
+        print("Topics already exist, skipping creation.")
+    else:
+        print(f"Failed to create topics: {e}")
+        exit(1)
+
+kafka.close()
+print("Kafka Admin closed successfully.")
